bcd80f7c4c7203937af2b888c54bbbfa6ef6b728
Rafbot5098 <dismas5098@gmail.com>
2025-04-21 12:43:52 +0200

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -28,7 +28,8 @@ public class Principal {
 	// 		}
 	// 	}
 	// } 
-	public void leerAsistentes(String cadena) throws IOException {
+	public Attendees leerAsistentes(String cadena) throws IOException {
+		
 		File f = new File(cadena);
 		Scanner nombre_f = new Scanner(f);
 
@@ -43,8 +44,9 @@ public class Principal {
 			frecuente = nombre_f.nextBoolean();
 			vip = nombre_f.nextBoolean();
 		}
+		Attendees att = new Attendees(nombre,dni,tarjeta,frecuente,null);
 	}
-	public void readArtist(String cadena) throws IOException {
+	public Artist[] readArtist(String cadena) throws IOException {
         //Opens given file and reads from it
 		File f = new File(cadena);
 		Scanner nombre_f = new Scanner(f);
@@ -56,9 +58,13 @@ public class Principal {
         int duration, maxAudience, memberNum, phoneNum;
         String name, genre; 
         boolean principal, dressing, merchandising, confirmedAssistance;
-		
-		
+		int count = 0;
+		while (nombre_f.hasNextLine()) {
+			count++;
+		}
+		Artist[] art = new Artist[count];
         //We use the while to say that while we have text, it has to keep reading, introducing every data in every line of the terminal
+		count = 0;
         while (nombre_f.hasNext()) {
             group = nombre_f.next().charAt(0);
             name = nombre_f.next();
@@ -68,8 +74,17 @@ public class Principal {
             duration = nombre_f.nextInt();
             maxAudience = nombre_f.nextInt();
             confirmedAssistance = nombre_f.nextBoolean();
-           
+			if (group == 'g') {
+				memberNum = nombre_f.nextInt();
+				merchandising = nombre_f.nextBoolean();
+				art[count] = new Group(name, genre, principal, maxAudience, duration, price, confirmedAssistance, memberNum, merchandising);
+			}
+			else{
+				dressing = nombre_f.nextBoolean();
+				phoneNum = nombre_f.nextInt();
+				art[count] = new Solo(name, genre, principal, maxAudience, duration, price, confirmedAssistance, dressing, phoneNum);
+			}
 	    }
-		Artist[]artists;
+		return art;
     } 
 }
