3dce1f348a261acb724c210a791e8f27abaa5fa1
Patricia Camacho Rivallo <60811137+paticama@users.noreply.github.com>
2025-05-05 17:52:01 +0200

    Main modularizado, añadidos algunos (pocos) comentarios

---
+++ Assignment3.java
@@ -0,0 +1,7 @@
+public class Assignment3 {
+    
+    public static void main(String[] args) {
+        SecurityCompany company1 = new SecurityCompany("EventoSeguro", 250.0);
+        System.out.println(company1.toString());
+    }
+}
