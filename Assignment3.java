ca816450e826cda6db4fcba66a9f0031dd5af4c3
Patricia Camacho Rivallo <60811137+paticama@users.noreply.github.com>
2025-05-05 17:31:02 +0200

    Delete Assignment3.java

--- Assignment3.java
+++
@@ -1,7 +0,0 @@
-public class Assignment3 {
-    
-    public static void main(String[] args) {
-        SecurityCompany company1 = new SecurityCompany("EventoSeguro", 250.0);
-        System.out.println(company1.toString());
-    }
-}
